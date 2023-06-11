ba1187a535e9bd5921c47dbc473b583677ba766e
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2023-06-11 14:23:43 -0500

    Update AssemblyInfo.cs

--- source/Bulb/Properties/AssemblyInfo.cs
+++ source/Bulb/Properties/AssemblyInfo.cs
@@ -2,35 +2,22 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("BULB")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BULB")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyTitle("Bright Utilitarian Luminescent Beacon (BULB)")]
+[assembly: AssemblyDescription("Allows the player to alter light colors in flight through a stock-alike interface on the right click menu.")]
+[assembly: AssemblyCompany("zer0Kerbal")]
+[assembly: AssemblyProduct("Bright Utilitarian Luminescent Beacon (BULB)")]
+[assembly: AssemblyCopyright("Copyright © 2014 Alshain, © 2020 Jiraiyah © 2023 zer0Kerbal")]
+[assembly: AssemblyTrademark("Trademark ™ 2014 Alshain, ™ 2023 zer0Kerbal")]
+[assembly: AssemblyCulture("Neutral")]
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+#if DEBUG 
+  [assembly: AssemblyConfiguration("Debug")]
+#else
+   [assembly: AssemblyConfiguration("Release")]
+#endif
+
 [assembly: Guid("81653d9c-aa98-4dbc-bdbf-9f309b58af2a")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.4.0")]
-[assembly: AssemblyFileVersion("1.3.4.0")]
+// [assembly: AssemblyVersion("1.3.4.0")]
+// [assembly: AssemblyFileVersion("1.3.4.0")]
